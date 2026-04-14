157fe91630d1a73c01747268839f5543a62c66d9
Martimex <kamilmarciniak533@wp.pl>
2026-04-14 16:18:23 +0200

    feat: handle fullscreen mode and browser resizing
    Enabled fullscreen mode on double click event.
    Added a webkit support for older browser versions
    that not supported full screen mode. Also the
    Three scene is fully responsive to the viewport
    or browser resizing.

--- script.js
+++ script.js
@@ -2,9 +2,29 @@ import * as THREE from "three";
 
 const canvas = document.querySelector('#webgl-canvas');
 const SIZES = {
-    width: 800,
-    height: 600,
-}
+    width: window.innerWidth,
+    height: window.innerHeight,
+};
+
+window.addEventListener('resize', () => {
+    SIZES.width = window.innerWidth;
+    SIZES.height = window.innerHeight;
+    camera.aspect = SIZES.width / SIZES.height;
+    camera.updateProjectionMatrix();
+    renderer.setSize(SIZES.width, SIZES.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+window.addEventListener('dblclick', () => {
+    const fullScreenEl = document.fullscreenElement || document.webkitFullscreenElement;
+    if(!fullScreenEl) {
+        if(canvas.requestFullscreen) canvas.requestFullscreen();
+        else if(canvas.webkitRequestFullscreen) canvas.webkitRequestFullscreen();
+    } else {
+        if(document.exitFullscreen) document.exitFullscreen();
+        else if(document.webkitExitFullscreen) document.webkitExitFullscreen();
+    }
+});
 
 const scene = new THREE.Scene();
 
@@ -24,4 +44,11 @@ const renderer = new THREE.WebGLRenderer({
 scene.add(sphereMesh);
 scene.add(camera);
 renderer.setSize(SIZES.width, SIZES.height);
-renderer.render(scene, camera);
+
+
+const loop = () => {
+    renderer.render(scene, camera);
+    window.requestAnimationFrame(loop);
+}
+
+loop();
