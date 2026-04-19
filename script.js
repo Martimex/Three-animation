0abba91d85ee1ef1c5a6a630ae8929496dae1016
Martimex <kamilmarciniak533@wp.pl>
2026-04-19 21:31:37 +0200

    feat: include controls to modify colors for cubes
    This little update give some more controls
    through the UI debug tooling. User can modify
    the hue value for the respective column of boxes,
    changing their color, but preserving saturation
    and lightness on the same level.

--- script.js
+++ script.js
@@ -13,15 +13,35 @@ const LILCONFIG = {
     animationType: {
         normal: () => CONFIG.CURRENT_ANIMATION_MODE = "normal",
         desync: () => CONFIG.CURRENT_ANIMATION_MODE = "desync",
+    },
+    colorsHue: {
+        "bar1": { hue: 0, rgb: null },
+        "bar2": { hue: 45, rgb: null },
+        "bar3": { hue: 90, rgb: null },
+        "bar4": { hue: 135, rgb: null },
+        "bar5": { hue: 180, rgb: null },
+        "bar6": { hue: 225, rgb: null },
+        "bar7": { hue: 270, rgb: null },
+        "bar8": { hue: 315, rgb: null },
     }
 }
 gui.add(LILCONFIG.animationType, "normal");
 gui.add(LILCONFIG.animationType, "desync");
+gui.add(LILCONFIG.colorsHue.bar1, "hue").min(0).max(360).step(1).name("Bar-1 color").onFinishChange((value) => changeBarColor({hueValue: value, barNo: 1}));
+gui.add(LILCONFIG.colorsHue.bar2, "hue").min(0).max(360).step(1).name("Bar-2 color").onFinishChange((value) => changeBarColor({hueValue: value, barNo: 2}));
+gui.add(LILCONFIG.colorsHue.bar3, "hue").min(0).max(360).step(1).name("Bar-3 color").onFinishChange((value) => changeBarColor({hueValue: value, barNo: 3}));
+gui.add(LILCONFIG.colorsHue.bar4, "hue").min(0).max(360).step(1).name("Bar-4 color").onFinishChange((value) => changeBarColor({hueValue: value, barNo: 4}));
+gui.add(LILCONFIG.colorsHue.bar5, "hue").min(0).max(360).step(1).name("Bar-5 color").onFinishChange((value) => changeBarColor({hueValue: value, barNo: 5}));
+gui.add(LILCONFIG.colorsHue.bar6, "hue").min(0).max(360).step(1).name("Bar-6 color").onFinishChange((value) => changeBarColor({hueValue: value, barNo: 6}));
+gui.add(LILCONFIG.colorsHue.bar7, "hue").min(0).max(360).step(1).name("Bar-7 color").onFinishChange((value) => changeBarColor({hueValue: value, barNo: 7}));
+gui.add(LILCONFIG.colorsHue.bar8, "hue").min(0).max(360).step(1).name("Bar-8 color").onFinishChange((value) => changeBarColor({hueValue: value, barNo: 8}));
 
 const CONFIG = {
     CURRENT_ANIMATION_MODE: "normal",
     objectsAmount: 8,
     positionDiff: 0.2,
+    defaultBaseColorSaturation: 55,
+    defaultBaseColorLightness: 55, 
     defaultRotationSpeed: 0.0066,
     onDesyncRotationSpeed: {
         "tier1": 0.0066,
@@ -118,6 +138,13 @@ function runAnimationFrame(animationMode) {
     }
 }
 
+function changeBarColor({hueValue, barNo}) {
+    const groupsToAffect = [customGroup_1, customGroup_2, customGroup_3, customGroup_4];
+    groupsToAffect.forEach((group, groupNo) => {
+        group.children[barNo - 1].material.color.set(`hsl(${hueValue}, ${CONFIG.defaultBaseColorSaturation + (5 * groupNo)}%, ${CONFIG.defaultBaseColorLightness + (5 * groupNo)}%)`);
+    });
+}
+
 function spawnGroups(amount) {
     for(let amountNo=0; amountNo<amount; amountNo++) {
         factory(amountNo);
@@ -128,7 +155,7 @@ function factory(repeatNo) {
     for(let i=0; i<CONFIG.objectsAmount; i++) {
         const newCube = new THREE.Mesh(
             new THREE.BoxGeometry(1, 1, 1),
-            new THREE.MeshBasicMaterial({color: `hsl(${i * (360 / CONFIG.objectsAmount)}, ${55 + (5 * repeatNo)}%, ${55 + (5 * repeatNo)}%)`})
+            new THREE.MeshBasicMaterial({color: `hsl(${LILCONFIG.colorsHue[`bar${i+1}`].hue}, ${CONFIG.defaultBaseColorSaturation + (5 * repeatNo)}%, ${CONFIG.defaultBaseColorLightness + (5 * repeatNo)}%)`})
         );
         newCube.position.x = posObj.xPos[(i) % posObj.xPos.length] / 2 ;
         newCube.position.y = posObj.yPos[(i) % posObj.yPos.length] / 2 ;
